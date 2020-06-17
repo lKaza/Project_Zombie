80122e0990b8da6d88cf5080724569d65bb0f7cf
lKaza <rodrigoguzman1202@gmail.com>
2020-06-17 18:23:00 -0400

    added vfx a thompson

--- Assets/EnemyHealth.cs
+++ Assets/EnemyHealth.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
+    [SerializeField] float maxHitPoints=100f;
+    private float currentHitPoints;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHitPoints = maxHitPoints;
+    }
+
+    public void TakeDmg(float dmg){
+        currentHitPoints -= dmg;
+
+        if(currentHitPoints <=0)
+        {
+            DeathSequence();
+        }
+    }
+
+    private void DeathSequence()
+    {
+        Destroy(this.gameObject);
     }
 
     // Update is called once per frame
--- Assets/Thompson.cs
+++ Assets/Thompson.cs
@@ -7,6 +7,10 @@ public class Thompson : MonoBehaviour
 {
     [SerializeField] Camera fpsCamera;
     [SerializeField] float range=100;
+    [SerializeField] float weaponDmg=10f;
+    [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] GameObject hitEffect;
+
     bool hitSomething;
     Animator myAnim;
     // Start is called before the first frame update
@@ -18,25 +22,49 @@ public class Thompson : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButton(0)){
+        if(Input.GetMouseButtonDown(0)){
             Shoot();
         }
-    }
+    } 
     private void LateUpdate() {
         myAnim.SetBool("BoltOpen", false);
     }
 
     private void Shoot()
     {
-        RaycastHit hit;
+
         AnimShoot();
+        VFXMuzzleFlash();
+        RayCastProcess();
+
+    }
+
+    private void VFXMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void RayCastProcess()
+    {
+        RaycastHit hit;
         hitSomething = Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range);
         if (hitSomething)
         {
-            print("i shoot dicks" + hit.transform.name);
+            CreateHitImpact(hit);
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) { return; }
+            target.TakeDmg(weaponDmg);
         }
+        else
+        {
+            return;
+        }
+    }
 
+    private void CreateHitImpact(RaycastHit hit)
+    {
+        GameObject  impact =Instantiate(hitEffect,hit.point,Quaternion.LookRotation(hit.normal));
+        Destroy(impact,0.5f);
     }
 
     private void AnimShoot()
